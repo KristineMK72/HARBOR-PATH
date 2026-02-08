584e09f7f1a237e5558795fd062b762d683d1db5
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-02-08 13:20:15 -0600

    Create page.tsx

---
+++ app/meetings/page.tsx
@@ -0,0 +1,10 @@
+import Container from "@/components/Container";
+
+export default function Meetings() {
+  return (
+    <Container>
+      <h1>Crow Wing County meetings</h1>
+      <p>Meeting list coming next.</p>
+    </Container>
+  );
+}
