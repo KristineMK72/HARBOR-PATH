f47d7f5894b3632a93598142704e11102bedd80d
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-02-08 13:19:01 -0600

    Create page.tsx

---
+++ app/families/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/Container";
+
+export default function Families() {
+  return (
+    <Container>
+      <h1>Families & loved ones</h1>
+      <p>
+        You didn’t cause this. You can’t control it. You can care for yourself.
+      </p>
+    </Container>
+  );
+}
