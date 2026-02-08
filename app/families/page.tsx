8878905f3623bd9eec74cf89a0cce454cf1aa43d
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-02-08 15:43:17 -0600

    Update page.tsx

--- app/families/page.tsx
+++ app/families/page.tsx
@@ -1,10 +1,10 @@
-import Container from "@/components/Container";
+import Container from "../../components/Container";
 
 export default function Families() {
   return (
     <Container>
       <h1>Families & loved ones</h1>
-      <p>
+      <p className="muted">
         You didn’t cause this. You can’t control it. You can care for yourself.
       </p>
     </Container>
