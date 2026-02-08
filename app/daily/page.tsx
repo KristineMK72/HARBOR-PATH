65a1efa889d442333b3eed1bdd371aab679094da
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-02-08 13:19:36 -0600

    Create page.tsx

---
+++ app/daily/page.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/Container";
+
+export default function Daily() {
+  return (
+    <Container>
+      <h1>Just for today</h1>
+      <p>
+        Just for today, I will try to live through this day only and not tackle
+        all my problems at once.
+      </p>
+    </Container>
+  );
+}
