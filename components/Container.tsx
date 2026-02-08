700f2b99ee1c5040ea94bb2590cad3ce8f5fc5dd
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-02-08 16:06:46 -0600

    Update Container.tsx

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,3 +1,5 @@
-export default function Container({ children }: { children: React.ReactNode }) {
+import type { ReactNode } from "react";
+
+export default function Container({ children }: { children: ReactNode }) {
   return <div className="container">{children}</div>;
-}p
+}
