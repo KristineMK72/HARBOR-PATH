828e5c109ca76d92dae398ef7dc6500d95c08f0b
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-02-08 13:18:27 -0600

    Create page.tsx

---
+++ app/help-now/page.tsx
@@ -0,0 +1,14 @@
+import Container from "@/components/Container";
+
+export default function HelpNow() {
+  return (
+    <Container>
+      <h1>Help right now</h1>
+      <ul>
+        <li>Call 911 if you are in immediate danger</li>
+        <li>Call or text 988 (U.S.)</li>
+        <li>SAMHSA Helpline: 1-800-662-4357</li>
+      </ul>
+    </Container>
+  );
+}
